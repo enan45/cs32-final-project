3f76c33ed1767c4dadea4bcf14f2b741efed0201
EmmanuelEnan <emmanuelenan45@gmail.com>
2026-04-23 09:58:14 +0000

    Thu, Apr 23, 2026, 5:58 AM -04:00

---
+++ circuits.py
@@ -0,0 +1,15 @@
+###555 Oscillator Circuit
+# 1 - GND
+# 2 — TRIG (trigger)
+# 3 — OUT (output)
+# 4 — RESET (tied to VCC for always-on)
+# 5 — CTL (control voltage)
+# 6 — THR (threshold)
+# 7 — DISCHG (discharge)
+# 8 — VCC
+# VCC connects pins 8 and 4 (and external stuff)
+# GND connects pin 1 (and external stuff)
+# DISCHG connects pin 7, R1, R2
+# THR/TRIG connects pins 2 and 6
+# CTL connects pin 5 and a cap
+# OUT connects pin 3 and a header
