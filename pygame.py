3f5873bc5d5629d524e53ba4676714b019948e8c
EmmanuelEnan <emmanuelenan45@gmail.com>
2026-04-23 11:28:30 +0000

    Thu, Apr 23, 2026, 7:28 AM -04:00

---
+++ pygame.py
@@ -0,0 +1,14 @@
+# initialize pygame window
+# load the circuit (build_555_timer)
+# pick source and target from pads dict
+#from youtube video on pygame....
+# create the stepped generator ----> currently reading on generator protocols...
+# for each (visited, frontier, came_from) yielded by the generator:
+#     handle window events (close button etc.)
+#     clear window and redraw all cells with current colors
+#     display current iteration count
+#     pygame.display.flip()
+#     small delay for animation speed
+# when generator returns the final path:
+#     draw the path in purple on top
+#     keep window open until user closes it
